6a89ae18c7f544449af77c25b0471c93f1634ef1
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-10 23:43:25 -0300

    Cambios al navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,16 +3,18 @@ import styles from './styles.module.css'
 
 export default function Navbar() {
   return (
-    <nav className={styles.nav}>
+    <header className={styles.header}>
       <Link to="/" className={styles.logo}>(LOGO)</Link>
 
-      <ul className={styles.links}>
-        <li><Link to='/gallery'>GALERY</Link></li>
-        <li><Link to='/search'>SEARCH</Link></li>
-        <li><Link to='/studio'>STUDIO</Link></li>
-      </ul>
+      <nav>
+        <ul className={styles.links}>
+          <li><Link to='/gallery'>GALLERY</Link></li>
+          <li><Link to='/search'>SEARCH</Link></li>
+          <li><Link to='/studio'>STUDIO</Link></li>
+        </ul>
+      </nav>
 
       <p>LOGIN</p>
-    </nav>
+    </header>
   )
 }
