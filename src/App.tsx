4b5f36e7eb0e0355f533697100375b1586c830cd
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-11 18:50:50 -0300

    Paginacion y Rutas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import Home from './pages/Home'
 import Gallery from './pages/Gallery'
 import Search from './pages/Search'
 import Studio from './pages/Studio'
+import Artwork from './pages/Artwork'
+import Profile from './pages/Profile'
+import Favorites from './pages/Favorites'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Settings from './pages/Settings'
 
 function App() {
   return (
@@ -11,9 +19,25 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/gallery">
+          <Route index element={<Gallery />}/>
+          <Route path=":artworkId" element={<Artwork />} />
+        </Route>
         <Route path="/search" element={<Search />} />
         <Route path="/studio" element={<Studio />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/auth">
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/profile">
+          <Route index element={<Profile />} />
+          {/* <Route path="edit" element={<Profile />} /> */}
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Route>
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   )
