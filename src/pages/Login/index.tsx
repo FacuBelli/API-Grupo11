5f83337e50e166f95e990738dc366b5b18d140ab
jazmartinezz <jazmartinez@uade.edu.ar>
2024-04-12 11:24:37 -0300

    cambios

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,37 @@
-import styles from "./styles.module.css"
-import { Link } from "react-router-dom"
-import Button from "../../components/Button/index"
+import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
+import Button from '../../components/Button/index'
 
 export default function Login() {
   return (
     <main>
-      <div className= {styles.container}>
-      <section className={styles.section}>
-        <h1> <span>LOGIN TO YOUR ACCOUNT </span></h1>
-        <form action="input" className={styles.form}>
-          <input className={styles.input} type="email" placeholder='email' />
-          <input className={styles.input} type="password" placeholder='password' />
-        </form>
-        <p>Forgot your password?</p> {/* hay que agregar el link para recuperar la contraseña */}
-        <Button to="/">Log In</Button>
-        <div className={styles.linea}></div>
-        <div>
-          <h2><span>new to our gallery</span></h2>
-          <p> <Link to='/auth/register' className={styles.signup}> Sign Up</Link> here to create an account and star exploring.</p>
-        </div>
-      </section>
+      <div className={styles.container}>
+        <section className={styles.section}>
+          <h1 className={styles.title}>
+            <span>LOGIN TO YOUR ACCOUNT </span>
+          </h1>
+          <form action='input' className={styles.form}>
+            <input className={styles.input} type='email' placeholder='email' />
+            <div className={styles.password}>
+              <input className={styles.input} type='password' placeholder='password' />
+              <Link to=''>Forgot your password?</Link> {/* hay que agregar el link para recuperar la contraseña */}
+            </div>
+          </form>
+          <Button to='/'>Log In</Button>
+          <div className={styles.linea}></div>
+          <div>
+            <h2>
+              <span>new to our gallery</span>
+            </h2>
+            <p>
+              <Link to='/auth/register' className={styles.signup}>
+                Sign Up
+              </Link>
+               {' '}here to create an account and star exploring.
+            </p>
+          </div>
+        </section>
       </div>
     </main>
-
   )
-
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,29 +1,42 @@
-import styles from "./styles.module.css"
-import { Link } from "react-router-dom"
-import Button from "../../components/Button/index"
+import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
+import Button from '../../components/Button/index'
 
 export default function Register() {
   return (
     <main>
       <div className={styles.container}>
-        <section className= {styles.section}>
-          <h1> <span>CREATE YOUR ACCOUNT </span></h1>
-          <form action="input" className={styles.form}>
-            <input className={styles.input} type="text" placeholder='FIRST NAME' />
-            <input className={styles.input} type="text" placeholder='LAST NAME' />
-            <input className={styles.input} type="email" placeholder='email' />
-            <input className={styles.input} type="password" placeholder='password' />
-            <input className={styles.input} type="password" placeholder='REPEAT PASSWORD' /> 
-            {/* <Button to="/" className={styles.button}>REGISTER</Button> */}
-            <div style={{ width: '100%', marginTop: '10px', display: 'flex', justifyContent: 'center' }}>
-           <Button to= "/">REGISTER</Button>
+        <section className={styles.section}>
+          <h1 className={styles.title}>
+          CREATE YOUR ACCOUNT
+          </h1>
+          <div className={styles.confirmation}>
+          <p>join out community of art enthusiast and creators by creating your account</p>
+          </div>
+          <form action='input' className={styles.form}>
+            <input className={styles.input} type='text' placeholder='FIRST NAME' />
+            <input className={styles.input} type='text' placeholder='LAST NAME' />
+            <input className={styles.input} type='email' placeholder='email' />
+            <input className={styles.input} type='password' placeholder='password' />
+            <input className={styles.input} type='password' placeholder='REPEAT PASSWORD' />
+            <div className={styles.button} >
+              <Button to='/'>REGISTER</Button>
             </div>
-            <p>by registering, you agree to pur terms of service and privacy politic </p>
+            <div className={styles.confirmation}>
+              <p>by registering, you agree to our terms of service and privacy policy </p>
+            </div>
           </form>
           <div className={styles.linea}></div>
           <div>
-            <h2><span>already have an account?</span></h2>
-            <p> <Link to='/auth/login' className={styles.login}> Log In</Link> here to access your account.</p>
+            <h2>
+              <span>already have an account?</span>
+            </h2>
+            <p>
+              <Link to='/auth/login' className={styles.login}>
+                Log In
+              </Link>
+              {' '} here to access your account.
+            </p>
           </div>
         </section>
       </div>
