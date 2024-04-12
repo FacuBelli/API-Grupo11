85f50c393f690ddc151d60fd24b3c9a509918d4e
FacuBelli <facundo_carabelli@hotmail.com>
2024-04-11 21:33:35 -0300

    Comienzo ArtWork

--- src/pages/Artwork/index.tsx
+++ src/pages/Artwork/index.tsx
@@ -1,7 +1,36 @@
+import styles from './styles.css'
+
+
 export default function Artwork() {
+  const product = 
+    {
+      id: 1,
+      title: "Sunset Dream",
+      artist: "Jane Doe",
+      description: "A beautiful sunset painting depicting the serene beauty of nature.",
+      image: "https://picsum.photos/500/500",
+      price: 200,
+      category: "Painting",
+    }
+
+
   return (
     <main>
     <h1>Artwork</h1>
+    <section id="imagePresentation">
+      <div className="description">
+        <h1 className={styles.}>{product.title}</h1>
+        <p>{product.artist}</p>
+        <p>{product.description}</p>
+        <p>u$s {product.price}</p>
+        <p>{product.category}</p>
+      </div>
+      <div className="imagen">
+        <img src={product.image} alt="" />
+      </div>
+
+    </section>
+
     </main>
   )
 }
