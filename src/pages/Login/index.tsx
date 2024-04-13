2a75d7a73d3f7468c525442402ab8442f46bdada
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-12 22:19:15 -0300

    Revision de css y estructura

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,33 +5,30 @@ import Button from '../../components/Button/index'
 export default function Login() {
   return (
     <main>
-      <div className={styles.container}>
-        <section className={styles.section}>
+      <section className={styles.section}>
+        <div className={styles.panel}>
           <h1 className={styles.title}>
-            <span>LOGIN TO YOUR ACCOUNT </span>
+            <span>LOGIN TO YOUR ACCOUNT</span>
           </h1>
-          <form action='input' className={styles.form}>
+          <form className={styles.form}>
             <input className={styles.input} type='email' placeholder='email' />
             <div className={styles.password}>
               <input className={styles.input} type='password' placeholder='password' />
               <Link to=''>Forgot your password?</Link> {/* hay que agregar el link para recuperar la contraseña */}
             </div>
+            <Button to='/'>Log In</Button>
           </form>
-          <Button to='/'>Log In</Button>
           <div className={styles.linea}></div>
-          <div>
+          <div className={styles.register}>
             <h2>
-              <span>new to our gallery</span>
+              <span>new to our gallery?</span>
             </h2>
-            <p>
-              <Link to='/auth/register' className={styles.signup}>
-                Sign Up
-              </Link>
-               {' '}here to create an account and star exploring.
-            </p>
+            <Link to='/auth/register' className={styles.link}>
+                <span>Sign Up</span> here to create an account and start exploring.
+            </Link>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </main>
   )
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -5,41 +5,38 @@ import Button from '../../components/Button/index'
 export default function Register() {
   return (
     <main>
-      <div className={styles.container}>
-        <section className={styles.section}>
+      <section className={styles.section}>
+        <div className={styles.panel}>
           <h1 className={styles.title}>
-          CREATE YOUR ACCOUNT
+            <span>CREATE YOUR ACCOUNT</span>
           </h1>
-          <div className={styles.confirmation}>
-          <p>join out community of art enthusiast and creators by creating your account</p>
-          </div>
-          <form action='input' className={styles.form}>
-            <input className={styles.input} type='text' placeholder='FIRST NAME' />
-            <input className={styles.input} type='text' placeholder='LAST NAME' />
-            <input className={styles.input} type='email' placeholder='email' />
-            <input className={styles.input} type='password' placeholder='password' />
-            <input className={styles.input} type='password' placeholder='REPEAT PASSWORD' />
-            <div className={styles.button} >
-              <Button to='/'>REGISTER</Button>
+          <p className={styles.text}>
+            join out community of art enthusiast and creators by creating your account
+          </p>
+          <form action="" className={styles.form}>
+            <div className={styles.inputRow}>
+              <input className={styles.input} type="text" placeholder="FIRST NAME" />
+              <input className={styles.input} type="text" placeholder="LAST NAME" />
             </div>
-            <div className={styles.confirmation}>
-              <p>by registering, you agree to our terms of service and privacy policy </p>
-            </div>
+            <input className={styles.input} type="email" placeholder="email" />
+            <input className={styles.input} type="password" placeholder="password" />
+            <input className={styles.input} type="password" placeholder="REPEAT PASSWORD" />
+            <Button to="/">REGISTER</Button>
           </form>
+          <p className={styles.confirmation}>
+            by registering, you agree to our terms of service and privacy policy{' '}
+          </p>
           <div className={styles.linea}></div>
-          <div>
+          <div className={styles.login}>
             <h2>
               <span>already have an account?</span>
             </h2>
-            <p>
-              <Link to='/auth/login' className={styles.login}>
-                Log In
-              </Link>
-              {' '} here to access your account.
-            </p>
+            <Link to="/auth/login" className={styles.link}>
+              <span>Log In</span> here to access your account.
+            </Link>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </main>
   )
 }
