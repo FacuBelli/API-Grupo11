41f1a0a71a64b332f6a61ead4ae016e5dc3920fa
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-13 16:51:32 -0300

    Componente Input implementacion de manejo de errores

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,61 +1,135 @@
 import { type SvgIconTypeMap } from '@mui/material'
 import { type OverridableComponent } from '@mui/material/OverridableComponent'
-import { type MouseEvent, createContext, useContext, useState } from 'react'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
+import {
+  type MouseEvent,
+  createContext,
+  useContext,
+  useState,
+  type ChangeEvent,
+  type DetailedHTMLProps
+} from 'react'
+import { Report, Visibility, VisibilityOff } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
+import { useForm } from '../../hooks/useForm'
 
-type Props = {
+export type InputAssistance = {
+  message: string
+  to: string
+}
+
+export type InputError = {
+  message: string
+  validate: (value: string, formElements?: HTMLFormControlsCollection) => boolean
+}
+
+interface Props
+  extends DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   type: 'text' | 'email' | 'password'
-  name: string
-  placeholder?: string
-  required?: boolean
   icon?: OverridableComponent<SvgIconTypeMap<object, 'svg'>> & {
     muiName: string
   } // MaterialUI Icon Component
   iconPosition?: 'start' | 'end'
-  assistance?: {
-    text: string
-    to: string
-  }
+  assistance?: InputAssistance
+  errorValidation?: InputError | InputError[]
 }
 
-const InputPropsContext = createContext<Pick<Props, 'name' | 'placeholder' | 'required'>>({
-  name: '',
-  placeholder: '',
-  required: false
+const InputPropsContext = createContext<
+  DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+>({})
+
+const InputErrorContext = createContext<
+  Pick<Props, 'errorValidation'> & {
+    setIsValid: (value: boolean) => void
+    setErrorMessage: (value: string) => void
+  }
+>({
+  errorValidation: undefined,
+  setIsValid: () => {},
+  setErrorMessage: () => {}
 })
 
 export default function Input({
   type,
-  name,
-  placeholder,
-  required,
   icon,
   iconPosition,
-  assistance
+  assistance,
+  errorValidation,
+  ...inputProps
 }: Props) {
   const Icon = icon
   const isIconStart = Icon !== undefined && (iconPosition === 'start' || iconPosition === undefined)
   const isIconEnd = Icon !== undefined && iconPosition === 'end'
 
+  const [isValid, setIsValid] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('error message')
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         {isIconStart && <Icon className={styles.icon} />}
-        <InputPropsContext.Provider value={{ name, placeholder, required }}>
-          {type === 'text' && <TextInput />}
-          {type === 'email' && <EmailInput />}
-          {type === 'password' && <PasswordInput />}
-        </InputPropsContext.Provider>
+        <InputErrorContext.Provider value={{ errorValidation, setIsValid, setErrorMessage }}>
+          <InputPropsContext.Provider value={{ ...inputProps }}>
+            {type === 'text' && <TextInput />}
+            {type === 'email' && <EmailInput />}
+            {type === 'password' && <PasswordInput />}
+          </InputPropsContext.Provider>
+        </InputErrorContext.Provider>
+        {!isValid && <Report className={styles.icon} />}
         {isIconEnd && <Icon className={styles.icon} />}
       </div>
+      {!isValid && <p className={styles.error}>{errorMessage}</p>}
       {assistance !== undefined && (
         <Link to={assistance.to} className={styles.assistance}>
-          {assistance.text}
+          {assistance.message}
         </Link>
       )}
     </div>
+  )
+}
+
+function DefaultInput({ type }: { type: 'text' | 'email' | 'password' }) {
+  const form = useForm()
+  const inputProps = useContext(InputPropsContext)
+  const { errorValidation, setIsValid, setErrorMessage } = useContext(InputErrorContext)
+  const [value, setValue] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const onBlur = () => {
+    if (errorValidation === undefined) return
+
+    const handleError = (error: InputError) => {
+      const isValid = error.validate(value, form)
+      console.log(isValid, error.message)
+      setIsValid(isValid)
+      setErrorMessage(isValid ? '' : error.message)
+      return isValid
+    }
+
+    if (!Array.isArray(errorValidation)) {
+      handleError(errorValidation)
+    } else {
+      for (let i = 0; i < errorValidation.length; i++) {
+        const valid = handleError(errorValidation[i])
+        if (!valid) break
+      }
+    }
+  }
+
+  return (
+    <>
+      <input
+        className={styles.input}
+        type={type}
+        {...inputProps}
+        value={value}
+        onChange={(e) => onChange(e)}
+        onBlur={onBlur}
+      />
+    </>
   )
 }
 
@@ -94,29 +168,6 @@ function PasswordInput() {
           <Visibility className={styles.icon} />
         )}
       </button>
-    </>
-  )
-}
-
-function DefaultInput({ type }: { type: 'text' | 'email' | 'password' }) {
-  const { name, placeholder, required } = useContext(InputPropsContext)
-  const [value, setValue] = useState('')
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-
-  return (
-    <>
-      <input
-        className={styles.input}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        required={required}
-        value={value}
-        onChange={(e) => onChange(e)}
-      />
     </>
   )
 }
