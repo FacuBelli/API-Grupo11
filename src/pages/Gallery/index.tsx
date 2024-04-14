c83f0f73a16ed88d4cca8e96a91bae39f8e6bbba
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-14 20:02:31 -0300

    Gallery titulos y estilos

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,6 +1,7 @@
 import ArtworkCard from '../../components/ArtworkCard'
 import Slider from '../../components/Slider'
 import CategoryCard from '../../components/CategoryCard'
+import styles from './styles.module.css'
 
 export default function Gallery() {
   const products = [
@@ -30,55 +31,58 @@ export default function Gallery() {
     }
   ]
   const category = [
-  {
-    title: 'LANDSCAPES',
-    url: 'https://learn.ncartmuseum.org/wp-content/uploads/2017/06/COLE-Romantic-Landscape-52_9_7-1024x743.jpg'
-  },
-  {
-    title: 'PORTRAITS',
-    url: 'https://i.pinimg.com/736x/15/69/f8/1569f89a84f73ed2b5648a127c5e9c86.jpg'
-  },
-  {
-    title: 'ABSTRACT',
-    url: 'https://artsandyou.net/cdn/shop/articles/el-arte-abstracto-lo-puede-hacer-cualquiera-467550.jpg?v=1701795623'
-  },
-  {
-    title: 'NATURE',
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2n8eSIaDliaScSaEHYAaRv7tKXiumjuSz85dBptoaSQ&s'
-  },
-  {
-    title: 'URBAN',
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSht0ZTJFkGryniuyMkSeSNTZcm6nzv-tiiKhId-Lqw8Q&s'
-  },
-  {
-    title: 'SURREAL',
-    url: 'https://concepto.de/wp-content/uploads/2018/02/surrealismo-obra-min-e1519327827783.jpg'
-  },
-]
+    {
+      title: 'LANDSCAPES',
+      url: 'https://learn.ncartmuseum.org/wp-content/uploads/2017/06/COLE-Romantic-Landscape-52_9_7-1024x743.jpg'
+    },
+    {
+      title: 'PORTRAITS',
+      url: 'https://i.pinimg.com/736x/15/69/f8/1569f89a84f73ed2b5648a127c5e9c86.jpg'
+    },
+    {
+      title: 'ABSTRACT',
+      url: 'https://artsandyou.net/cdn/shop/articles/el-arte-abstracto-lo-puede-hacer-cualquiera-467550.jpg?v=1701795623'
+    },
+    {
+      title: 'NATURE',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2n8eSIaDliaScSaEHYAaRv7tKXiumjuSz85dBptoaSQ&s'
+    },
+    {
+      title: 'URBAN',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSht0ZTJFkGryniuyMkSeSNTZcm6nzv-tiiKhId-Lqw8Q&s'
+    },
+    {
+      title: 'SURREAL',
+      url: 'https://concepto.de/wp-content/uploads/2018/02/surrealismo-obra-min-e1519327827783.jpg'
+    }
+  ]
 
   return (
     <main>
-      <h1>Gallery</h1>
-      <Slider>
-        {products.map((item, i) => (
-          <ArtworkCard
-            id={item.id}
-            title={item.title}
-            price={item.price}
-            image={item.url}
-            key={i}
-          />
-        ))}
-      </Slider>
-      <Slider>
-        {category.map((item, i) => (
-          <CategoryCard
-            title={item.title}
-            url={item.url}
-            key={i}
-          />
-        ))}
-      </Slider>
+      <section className={styles.category}>
+        <h1 className={styles.categoryTitle}>
+          EXPLORE UNIQUE AI ART <span>IN EVERY CATEGORY</span>
+        </h1>
+        <Slider>
+          {category.map((item, i) => (
+            <CategoryCard title={item.title} url={item.url} key={i} />
+          ))}
+        </Slider>
+      </section>
+      <section className={styles.popular}>
+        <h2 className={styles.popularTitle}><span>MOST</span> POPULAR</h2>
+        <Slider>
+          {products.map((item, i) => (
+            <ArtworkCard
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              image={item.url}
+              key={i}
+            />
+          ))}
+        </Slider>
+      </section>
     </main>
   )
 }
