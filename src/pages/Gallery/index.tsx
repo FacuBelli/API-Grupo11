131c81bd63920b3bdc10d585bd3ad8f65ed93b1c
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-23 16:25:27 -0300

    Hook usePopular para artworks

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -2,34 +2,12 @@ import ArtworkCard from '../../components/ArtworkCard'
 import Slider from '../../components/Slider'
 import CategoryCard from '../../components/CategoryCard'
 import styles from './styles.module.css'
+import usePopular from '../../hooks/usePopular'
 
 export default function Gallery() {
-  const products = [
-    {
-      id: '0',
-      title: 'Producto 1',
-      price: 100,
-      url: '/assets/img/example-horizontal.png'
-    },
-    {
-      id: '1',
-      title: 'Producto 2',
-      price: 12,
-      url: '/assets/img/example-horizontal.png'
-    },
-    {
-      id: '2',
-      title: 'Producto 3',
-      price: 140,
-      url: '/assets/img/example-horizontal.png'
-    },
-    {
-      id: '3',
-      title: 'Producto 4',
-      price: 20,
-      url: '/assets/img/example-horizontal.png'
-    }
-  ]
+  const popular = usePopular()
+  console.log(popular)
+
   const category = [
     {
       title: 'LANDSCAPES',
@@ -72,12 +50,12 @@ export default function Gallery() {
       <section className={styles.popular}>
         <h2 className={styles.popularTitle}><span>MOST</span> POPULAR</h2>
         <Slider>
-          {products.map((item, i) => (
+          {popular.map((item, i) => (
             <ArtworkCard
               id={item.id}
               title={item.title}
               price={item.price}
-              image={item.url}
+              image={item.image}
               key={i}
             />
           ))}
@@ -86,12 +64,12 @@ export default function Gallery() {
       <section className={styles.popular}>
         <h2 className={styles.popularTitle}><span>MOST</span> RECENT</h2>
         <Slider>
-          {products.map((item, i) => (
+          {popular.map((item, i) => (
             <ArtworkCard
               id={item.id}
               title={item.title}
               price={item.price}
-              image={item.url}
+              image={item.image}
               key={i}
             />
           ))}
