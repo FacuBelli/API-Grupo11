483898865f380f42d9f81c79283f25bd13659f60
FacuBelli <facundo_carabelli@hotmail.com>
2024-04-24 10:09:46 -0300

    Continuacion Cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,9 @@
 import db from '../../utils/database'
 import styles from './styles.module.css'
+import Button from '../../components/Button'
+import Slider from '../../components/Slider'
+import CategoryCard from '../../components/CategoryCard'
+
 
 export default function Cart() {
   const userId = 1
@@ -11,21 +15,24 @@ export default function Cart() {
   
   return (
     <main>
-        <section>
-        
-          
+      <section className={styles.greatContainer}>
+          <section className={styles.pepito}>
+            <h2 className={styles.titles}>Your Shopping Cart</h2>
+            <p className={styles.description}>Welcome to your shopping cart! Here you can review and manage all the items you've added.</p>
+          </section>
 
-          
-        <section className={styles.pepito}>
-          <h2 className={styles.titles}>Your Shopping Cart</h2>
-          <p className={styles.description}>Welcome to your shopping cart! Here you can review and manage all the items you've added.</p>
+          <section className={styles.rumba}>
+            <h2>Manage your Cart "aca van los items"</h2>
+            {artworks.map((artwork, i) => (
+              <div>
+                <img src={artwork.image} alt="" />
+                <p>{artwork.title}</p>title=  key={i}
+              </div>
+            
+          ))}
         </section>
-
-        <section className={styles.rumba}>
-          <h2>Manage your Cart "aca van los items"</h2>
-
       </section>
-    </section>
+    
     </main>
   )
 }
