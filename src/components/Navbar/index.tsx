aa98ca46c185d6c86364163b67889c3cdec3126f
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-24 10:09:27 -0300

    Profile menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
+import useAuth from '../../hooks/useAuth'
 
 export default function Navbar() {
+  const { isLogged } = useAuth()
+
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.logo}>(LOGO)</Link>
@@ -14,7 +17,27 @@ export default function Navbar() {
         </ul>
       </nav>
 
-      <Link to='/auth/login'>LOGIN</Link>
+      {
+        isLogged ? <Profile /> : <Link to='/auth/login'>LOGIN</Link>
+      }
+      
     </header>
+  )
+}
+
+function Profile () {
+  const { user } = useAuth()
+  return (
+    <div className={styles.profile}>
+      <Link to='/profile'>MR. {user?.first_name}</Link>
+      <div className={styles.menu}>
+        <ul><Link to='/profile'>PROFILE</Link></ul>
+        <ul><Link to='/favorites'>FAVORITES</Link></ul>
+        <ul><Link to='/cart'>CART</Link></ul>
+        <ul><Link to='/profile/edit'>BECOME A CREATOR</Link></ul>
+        <ul><Link to='/settings'>SETTINGS</Link></ul>
+        <ul><Link to=''>LOG OUT</Link></ul>
+      </div>
+    </div>
   )
 }
