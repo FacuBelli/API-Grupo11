0663b8acb27c52af2b495d581aff71d6f74fff71
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-04-25 17:41:21 -0300

    useAuth hook register y logout con redux

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,31 +2,77 @@ import { useDispatch, useSelector } from 'react-redux'
 import { type RootState } from '../redux'
 import db from '../utils/database'
 import { validateInput } from '../utils/validation'
-import { userLogin } from '../redux/actions/userActions'
-
+import { userLogin, userLogout } from '../redux/actions/userActions'
 
 const useAuth = () => {
   const dispatch = useDispatch()
 
-  return {
-    ...useSelector((state: RootState) => state.user),
-    login: (email: string, password: string) => {
-      const { isValid: isValidEmail, message: messageEmail } = validateInput('email', email)
-      if (!isValidEmail) throw new Error(messageEmail)
+  const { user, isLogged } = useSelector((state: RootState) => state.user)
 
-      const { isValid: isValidPassword, message: messagePassword } = validateInput('password', password)
-      if (!isValidPassword) throw new Error(messagePassword)
-
-      const user = db.users.find((user) => user.email === email && user.password === password)
-      if (user === undefined) throw Error('User not found.')
-
-      dispatch(userLogin(user))
-    },
-    register: () => {
-      // register
+  const login = (email: string, password: string) => {
+    const emailValidation = validateInput('email', email)
+    if (!emailValidation.isValid) {
+      throw new Error(emailValidation.message)
     }
+
+    // const passwordValidation = validateInput('password', password)
+    // if (!passwordValidation.isValid) {
+    //   throw new Error(emailValidation.message)
+    // }
+
+    const user = db.users.find((user) => user.email === email && user.password === password)
+    if (user === undefined) throw new Error('User not found.')
+
+    dispatch(userLogin(user))
+  }
+
+  const register = (firstName: string, lastName: string, email: string, password: string) => {
+    const firstNameValidation = validateInput('firstName', firstName)
+    console.log(firstNameValidation)
+    if (!firstNameValidation.isValid) {
+      throw Error(firstNameValidation.message)
+    }
+
+    const lastNameValidation = validateInput('lastName', lastName)
+    if (!lastNameValidation.isValid) {
+      throw new Error(lastNameValidation.message)
+    }
+
+    const emailValidation = validateInput('email', email)
+    if (!emailValidation.isValid) {
+      throw new Error(emailValidation.message)
+    }
+
+    const passwordValidation = validateInput('password', password)
+    if (!passwordValidation.isValid) {
+      throw new Error(passwordValidation.message)
+    }
+
+    const user = {
+      id: db.users.length,
+      biography: '',
+      email,
+      password,
+      first_name: firstName,
+      last_name: lastName,
+      is_artist: false
+    }
+
+    db.users.push(user)
+    dispatch(userLogin(user))
+  }
+
+  const logout = () => {
+    dispatch(userLogout())
+  }
+
+  return {
+    user,
+    isLogged,
+    login,
+    register,
+    logout
   }
 }
-
 
 export default useAuth
