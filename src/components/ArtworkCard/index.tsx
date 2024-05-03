8149dbb63c267bec605536d7995f41d22f9258a9
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-05-03 17:58:52 -0300

    ArtworkCard cambios diseño

--- src/components/ArtworkCard/index.tsx
+++ src/components/ArtworkCard/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
 import FavoriteButton from '../FavoriteButton'
 import type { Artwork } from '../../types/database'
+import { formatPrice } from '../../utils/format'
 
 interface Props {
   artwork: Artwork
@@ -9,14 +10,17 @@ interface Props {
 
 export default function ArtworkCard({ artwork }: Props) {
   return (
-    <Link to={`/gallery/${artwork.id}`} className={artwork.is_sold ? `${styles.card} ${styles.disabled}` : styles.card}>
+    <Link
+      to={`/gallery/${artwork.id}`}
+      className={artwork.is_sold ? `${styles.card} ${styles.disabled}` : styles.card}
+    >
       <div className={styles.favorite}>
-        <FavoriteButton id={artwork.id} />
+        {!artwork.is_sold && <FavoriteButton id={artwork.id} />}
       </div>
       <img className={styles.image} src={artwork.image} />
       <div className={styles.dataContainer}>
         <h3 className={styles.title}>{artwork.title}</h3>
-        <p className={styles.price}>${artwork.price}</p>
+        <p className={styles.price}>{formatPrice(artwork.price ?? 0)}</p>
       </div>
     </Link>
   )
