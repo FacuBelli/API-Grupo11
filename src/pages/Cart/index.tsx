c2ce9070ee6cd84411d45b8fc86934fd82da90c6
FacuBelli <facundo_carabelli@hotmail.com>
2024-05-04 17:14:39 -0300

    Cambios en seccion cart. Inicio Cart Summary

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import db from '../../utils/database';
 import styles from './styles.module.css';
+import { formatPrice } from '../../utils/format';
+
 
 export default function Cart() {
   const userId = 1;
@@ -31,7 +33,7 @@ export default function Cart() {
     const updatedCart = cart.map((item) => {
       if (item.artwork_id === artworkId && item.quantity > 2) {
         return { ...item, quantity: item.quantity - 1 };
-        
+
       }
 
       return item;
@@ -51,42 +53,57 @@ export default function Cart() {
 
   return (
     <main>
-      <section className={styles.greatContainer}>
-        <section className={styles.pepito}>
-          <h2 className={styles.titles}>Your Shopping Cart</h2>
-          <p>
-            Welcome to your shopping cart! Here you can review and manage all the items you've added.
-          </p>
-        </section>
-
-        <section className={styles.rumba}>
-          <h2>Manage your Cart</h2>
-          <div className={styles.productView}>
-          {artworks.map((artwork, i) => (
-            <div className={styles.container} key={i}>
-              <img src={artwork.image} alt="" />
-              <div className={styles.productInfo}>
-                <p className={styles.title}>{artwork.title}</p>
-                <p className={styles.description}>{artwork.description}</p>
-                <p>{artwork.price}</p>
-                <div className={styles.border}></div>
-              </div>
-              <div className={styles.buttonContainer}>
-                
-                <button className={styles.button} onClick={() => decreaseQuantity(artwork.id)}>-</button>
-                <span>{cart.find((item) => item.artwork_id === artwork.id)?.quantity || 1}</span>
-                <button className={styles.button} onClick={() => increaseQuantity(artwork.id)}>+</button>
-                
-              </div>
-              <button className={styles.button} onClick={() => removeFromCart(artwork.id)}>Remove</button> 
-               
-            </div>
-            
-          ))}
-          </div>
-          <button className={styles.remove}onClick={removeAllItems}>Remove All Items</button>
-        </section>
+      <section className={styles.pepito}>
+        <h2 className={styles.titles}>Your Shopping Cart</h2>
+        <p>
+          Welcome to your shopping cart! Here you can review and manage all the items you've added.
+        </p>
+        <h2>Manage your Cart</h2>
       </section>
+      <section className={styles.greatContainer}>
+
+
+        <div className={styles.rumba}>
+
+          <div className={styles.productView}>
+            {artworks.map((artwork, i) => (
+              <div className={styles.container} key={i}>
+                <img className={styles.image} src={artwork.image} alt="" />
+                <div className={styles.productInfo}>
+                  <p className={styles.title}>{artwork.title}</p>
+                  <p className={styles.description}>{artwork.description}</p>
+                  <p className={styles.price}>{formatPrice(artwork.price!)}</p>
+                  <div className={styles.border}></div>
+                </div>
+
+                <div className={styles.productAcomodo}>
+                  <div className={styles.buttonContainer}>
+
+                    <button className={styles.button} onClick={() => decreaseQuantity(artwork.id)}>-</button>
+                    <span>{cart.find((item) => item.artwork_id === artwork.id)?.quantity || 1}</span>
+                    <button className={styles.button} onClick={() => increaseQuantity(artwork.id)}>+</button>
+
+                  </div>
+                  <button className={styles.button} onClick={() => removeFromCart(artwork.id)}>Remove</button>
+                </div>
+
+
+
+              </div>
+
+
+
+            ))}
+          </div>
+
+          <button className={styles.remove} onClick={removeAllItems}>Remove All Items</button>
+        </div>
+        <div className={styles.cartSummary}>
+          <h3>Cart Summary</h3>
+        </div>
+
+      </section>
+
     </main>
   );
 }
