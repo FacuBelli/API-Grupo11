087076a0316c301342f43369f82b201e62d0f956
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-05-05 19:50:33 -0300

    Search filtrado por categorías

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -5,10 +5,9 @@ import Filter from '../../components/Filter'
 import ImageGallery from '../../components/ImageGallery'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../../redux'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Tune } from '@mui/icons-material'
 import { useSearchParams } from 'react-router-dom'
-import type { Artwork } from '../../types/database'
 
 const filterOptions = [
   {
@@ -41,35 +40,35 @@ export default function Search() {
   const [searchParams, setSearchParams] = useSearchParams()
   const artworks = useSelector((state: RootState) => state.artwork.artworks)
   const [isFiltersOpen, setIsFilterOpen] = useState(searchParams.size !== 0)
-  // const filteredArtworks = useMemo(
-  //   () =>
-  //     artworks.filter((artwork) => {
-  //       // Check if the artwork matches all selected options
-  //       return Array.from(searchParams.entries()).every(([selectedOption, selectedValue]) => {
-  //         const propertyValue = artwork[selectedOption as keyof typeof artwork]
-  //         if (Array.isArray(propertyValue)) {
-  //           return propertyValue.includes(selectedValue)
-  //         } else if (propertyValue === selectedValue) {
-  //           return true
-  //         } else if (selectedOption === 'price range' && propertyValue) {
-  //           switch (selectedValue) {
-  //             case 'Under $50':
-  //               return propertyValue < 50
-  //             case '$50 - $100':
-  //               return propertyValue >= 50 && propertyValue < 100
-  //             case '$100 - $200':
-  //               return propertyValue >= 100 && propertyValue < 200
-  //             case '$200+':
-  //               return propertyValue >= 200
-  //             default:
-  //               return false
-  //           }
-  //         }
-  //         return false
-  //       })
-  //     }),
-  //   [artworks, searchParams]
-  // )
+  const filteredArtworks = useMemo(
+    () =>
+      artworks.filter((artwork) => {
+        return Array.from(searchParams.entries()).every(([selectedOption, selectedValue]) => {
+          const propertyValue = artwork[selectedOption as keyof typeof artwork]
+          if (Array.isArray(propertyValue)) {
+            return propertyValue.some((prop) => prop.name === selectedValue)
+          } else if (propertyValue === selectedValue) {
+            return true
+          } else if (selectedOption === 'price range') {
+            const price = artwork.price ?? 0
+            switch (selectedValue) {
+              case 'Under $50':
+                return price < 50
+              case '$50 - $100':
+                return price >= 50 && price < 100
+              case '$100 - $200':
+                return price >= 100 && price < 200
+              case '$200+':
+                return price >= 200
+              default:
+                return false
+            }
+          }
+          return false
+        })
+      }),
+    [artworks, searchParams]
+  )
 
   return (
     <main>
@@ -111,7 +110,7 @@ export default function Search() {
           </div>
         </aside>
         <div className={styles.galleryContainer}>
-          <ImageGallery artworks={artworks} />
+          <ImageGallery artworks={filteredArtworks} />
         </div>
       </section>
     </main>
